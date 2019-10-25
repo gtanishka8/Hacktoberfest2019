f2702831716d714d5b9e064cc9d10afe64a45f30
gtanishka8 <gtanishka8@gmail.com>
2019-10-25 17:18:55 +0530

    palindrome code editted

--- Python/pallindrom.py
+++ Python/pallindrom.py
@@ -1,11 +1,6 @@
 n=int(input("Enter number:"))
-temp=n
-rev=0
-while(n>0):
-    dig=n%10
-    rev=rev*10+dig
-    n=n//10
-if(temp==rev):
+m=str(n)
+if(m==m[::-1]):
     print("The number is a palindrome!")
 else:
     print("The number isn't a palindrome!")
